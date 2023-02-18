72fbe9aed11860cee0c7df2527fe353b0eae58e2
Jasper Huting <jasper@ixxi.com>
2023-02-18 21:34:14 +0100

    deleted 10 minutes

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,4 +1,8 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +11,21 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.scheduledFunction = functions.pubsub
+  .schedule("@monthluy").onRun(() => {
+    admin
+      .firestore()
+      .collection("mail")
+      .add({
+        to: "jasperhuting+test@gmail.com",
+        message: {
+          subject: "Hello from Firebase!",
+          text: "This is the plaintext section of the email body.",
+          html: "This is the <code>HTML</code> section of the email body.",
+        },
+      })
+      .then(() => {
+        console.log("Queued email for delivery!");
+      });
+  });
